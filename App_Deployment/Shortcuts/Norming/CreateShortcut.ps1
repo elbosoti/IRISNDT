c8d5311737c907faad08565104eabb67c9b28b24
elbosoti <bpblinde@live.nl>
2022-05-09 13:47:48 -0600

    Norming Icon Fixed

--- App_Deployment/Shortcuts/Norming/CreateShortcut.ps1
+++ App_Deployment/Shortcuts/Norming/CreateShortcut.ps1
@@ -2,6 +2,7 @@
 # Script Name:     Create-Shortcut.ps1
 # Description:     Will create a shortcut on the public desktop for The specified site or app
 # Date: 2022/05/06 Bob Blinde
+#powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File CreateShortcut.ps1
 #=============================================================================================================================
 #Manual Variables
 $ShortcutName = "Norming"
@@ -25,7 +26,9 @@ $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut($destinationPath)
 
 if ($IconName){
-    Copy-Item "icons" -Destination "C:\ISIT\Applications\icons" -Force -Recurse
+    $IconPath = "C:\ISIT\Applications\icons"
+    [System.IO.Directory]::CreateDirectory($IconPath)
+    Copy-Item $IconName -Destination "C:\ISIT\Applications\icons" -Force
     $IconFile = "C:\ISIT\Applications\icons\$IconName"
     $Shortcut.IconLocation = $IconFile
 }
