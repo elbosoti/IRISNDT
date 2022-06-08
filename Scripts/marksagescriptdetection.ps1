fd0518417a8fd0ecf7afac6622c2b6b8e56b6b4a
Bob Blinde <bpblinde@live.nl>
2022-06-08 16:42:19 -0600

    Marks sage script

---
+++ Scripts/marksagescriptdetection.ps1
@@ -0,0 +1,28 @@
+# The purpose of this script is to remove a registry folder if the URL property of the folder contains any entry of the $removeUrls
+$removeUrls = @("https://ca-edm-sage.irisndt.com", "https://sage.irisndt.com")
+$path = "Registry::HKEY_CURRENT_USER\Software\Microsoft\Workspaces\Feeds"
+# Proceeds only if the $path exists
+if(Test-Path $path)
+{
+    $feeds = Get-ChildItem -Path $path
+    # Loop through each folder in the $path
+    foreach($feed in $feeds){
+        $feed
+        # Get properties of the folder
+        $feedProperties = Get-ItemProperty $feed.PSPath
+        # Continue only if the URL property exists
+        if($feedProperties.URL.Length -gt 0){
+            $feedProperties.URL
+            # Loop through each url to remove for comparison
+            foreach($removeURL in $removeUrls){
+                # If the URL contains any of the remove URL, remove the folder
+                if($feedProperties.URL.toLower().Contains($removeURL)){
+                    Write-Host "Match is still found"
+                    exit 1
+                }
+            }
+        }
+    }
+}
+Write-Host "No matches were found"
+exit 0
